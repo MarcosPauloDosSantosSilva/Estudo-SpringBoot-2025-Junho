43f915f487d1d77e1dccb73bd1a6b218b2cbb8db
MarcosPauloDosSantosSilva <mpssss@hotmail.com>
2025-06-01 22:28:01 -0300

    Criada a entidade do banco de dados apartir do CadastroModel com JPA

--- src/main/java/com/mpss/Curso/Spring/Web/Thymileaf/CadastroController.java
+++ src/main/java/com/mpss/Curso/Spring/Web/Thymileaf/CadastroController.java
@@ -5,7 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PrimeiroController {
+public class CadastroController {
+
 
     @RequestMapping("/")
     public String primeiroAppWeb(Model model){
